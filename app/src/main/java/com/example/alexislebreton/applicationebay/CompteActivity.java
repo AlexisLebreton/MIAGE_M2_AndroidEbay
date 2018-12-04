65d6bf1e5602955c77bc0bdd723f7748bca15f68
Entrax643 <entrax.643@gmail.com>
2018-12-04 16:42:29 +0100

    internet

--- app/src/main/java/com/example/alexislebreton/applicationebay/CompteActivity.java
+++ app/src/main/java/com/example/alexislebreton/applicationebay/CompteActivity.java
@@ -1,20 +1,37 @@
 package com.example.alexislebreton.applicationebay;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.alexislebreton.applicationebay.model.User;
+import com.example.alexislebreton.applicationebay.rest.ApiClient;
+import com.example.alexislebreton.applicationebay.rest.ApiInterface;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class CompteActivity extends AppCompatActivity {
+    private static final String TAG = CompteActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compte);
 
-        final Button sinscrire_btn_sinscrire = (Button)findViewById(R.id.compte_btn_enregistrer);
+        final Button sinscrire_btn_sinscrire = (Button) findViewById(R.id.compte_btn_enregistrer);
         sinscrire_btn_sinscrire.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -31,11 +48,34 @@ public class CompteActivity extends AppCompatActivity {
                 final EditText compte_et_adresse = (EditText) findViewById(R.id.compte_et_adresse);
                 String adresse = compte_et_adresse.getText().toString();
 
-                String idUtilisateur= getIntent().getStringExtra("UTILISATEUR_ID");
+                String idUtilisateur = getIntent().getStringExtra("UTILISATEUR_ID");
+                Log.d(TAG, "idUtilisateur : " + idUtilisateur);
 
-                if (idUtilisateur=="NEW"){
-                    //TODO new util POST
-                }else{
+                ApiInterface apiService =
+                        ApiClient.getClient().create(ApiInterface.class);
+                Log.d(TAG, "idUtilisateur.equals(\"NEW\") : " + idUtilisateur.equals("NEW"));
+
+                if (idUtilisateur.equals("NEW")) {
+                    User user = new User(login, motdepasse, nom, prenom, email, adresse, "");
+                    Log.e(TAG, " User : " + user.toString());
+                    Gson gson = new Gson();
+                    String json = gson.toJson(user);
+                    Log.e(TAG, " userJSON : " + user.toString());
+                    Call<User> addNewUser = apiService.addNewUser(user);
+                    addNewUser.enqueue(new Callback<User>() {
+                        @RequiresApi(api = Build.VERSION_CODES.N)
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            Log.d(TAG, "Response : " + response.body());
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+                            // Log error here since request failed
+                            Log.e(TAG, "onFailure Erreur : " + t.toString());
+                        }
+                    });
+                } else {
                     //TODO new util UPDATE
                 }
                 finish();
